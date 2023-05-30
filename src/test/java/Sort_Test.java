f710f414b0c05565cab36fe36ae0f464ff94175b
OmarTammam25 <omartammam25@gmail.com>
2023-05-30 14:25:12 +0300

    Add tests for worst and best case scenarios

--- src/test/java/Sort_Test.java
+++ src/test/java/Sort_Test.java
@@ -107,6 +107,18 @@ public class Sort_Test {
         Assert.assertArrayEquals(expected_Array.toArray(), result.toArray());
     }
 
+    @Test
+    public void test_simpleSortingNegativeNumbers(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = -100; i <= 100; i++)
+            expected.add(i);
+        Collections.shuffle(expected);
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Simple_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
 //    @Test
 //    public void test_simpleSorting1e5NumbersFromFile()
 //    {
@@ -156,6 +168,18 @@ public class Sort_Test {
         expected_Array.sort(Integer::compareTo);
 
         Assert.assertArrayEquals(expected_Array.toArray(), result.toArray());
+    }
+
+    @Test
+    public void test_efficientSortingNegativeNumbers(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = -100; i <= 100; i++)
+            expected.add(i);
+        Collections.shuffle(expected);
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Efficient_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
     }
 
 //    @Test
@@ -216,6 +240,18 @@ public class Sort_Test {
 
         Assert.assertArrayEquals(expected_Array.toArray(), result.toArray());
     }
+
+    @Test
+    public void test_nonComparisonSortingNegativeNumbers(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = -100; i <= 100; i++)
+            expected.add(i);
+        Collections.shuffle(expected);
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Non_Comparison_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
 //    @Test
 //    public void test_nonComparisonSorting1e5NumbersFromFile(){
 //        Sort_Array sortObject = new Sort_Array(path + "\\1e5.txt");
@@ -246,5 +282,116 @@ public class Sort_Test {
 //        nums.sort(Integer::compareTo);
 //        Assert.assertArrayEquals(nums.toArray(), res.toArray());
 //    }
+
+    @Test
+    public void test_simpleSortingBestCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = 1; i <= 1e4; i++)
+            expected.add(i);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Simple_Sort(Sorting_Choice.Final_Result).get(0);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    public void test_simpleSortingWorstCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = (int)1e4; i > 0; i--)
+            expected.add(i);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Simple_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    public void test_simpleSortingAverageCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = 1; i <= 1e4; i++)
+            expected.add(i);
+        Collections.shuffle(expected);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Simple_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    // in merge sort, the best case is still O(n lg n)
+    @Test
+    public void test_efficientSortingBestCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = 1; i <= 1e4; i++)
+            expected.add(i);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Efficient_Sort(Sorting_Choice.Final_Result).get(0);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    // in merge sort, the worst case is still O(2*n lg n)
+    @Test
+    public void test_efficientSortingWorstCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = (int)1e4; i > 0; i--)
+            expected.add(i);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Efficient_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    @Test
+    public void test_efficientSortingAverageCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = 1; i <= 1e4; i++)
+            expected.add(i);
+        Collections.shuffle(expected);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Efficient_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    // best case in counting sort is still O(n) but it is not dependent on the input
+    @Test
+    public void test_nonComparsionSortingBestCase(){
+        List<Integer> expected = new ArrayList<>();
+        for(int i = 1; i <= 1e4; i++)
+            expected.add(i);
+
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Non_Comparison_Sort(Sorting_Choice.Final_Result).get(0);
+
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
+
+    // worst case in counting sort is when you give it a large range of numbers
+    @Test
+    public void test_nonComparsionSortingWorstCase(){
+        List<Integer> expected = new ArrayList<>();
+        expected.add((int)1e8);
+        expected.add((int)-1e7);
+        expected.add((int)-1e6);
+        expected.add((int)-1e5);
+        expected.add((int)-1e4);
+        expected.add((int)-1e3);
+        expected.add((int)-1e2);
+        expected.add((int)-10);
+        expected.add((int)1e7);
+        expected.add((int)1e6);
+        expected.add((int)1e5);
+        expected.add((int)1e4);
+        expected.add((int)1e3);
+        expected.add((int)1e2);
+        Sort_Array sortObject = new Sort_Array(expected);
+        List<Integer> result = sortObject.Non_Comparison_Sort(Sorting_Choice.Final_Result).get(0);
+        expected.sort(Integer::compareTo);
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
+    }
 
 }
